e561b4b8b12408fc20569104dcbcee6a775c89c4
Mulaudzi <114157158+Mjlee079@users.noreply.github.com>
2024-09-03 10:27:58 +0200

    Add files via upload

---
+++ Launch countdown timer/app.js
@@ -0,0 +1,36 @@
+const countdown=()=>{
+
+
+    const endDate = new Date ("January 1 ,2022 00:00:00").getTime()
+    const now = new Date().getTime()
+  
+    const difference = endDate-now
+  
+    const seconds = 1000;
+    const minutes = seconds*60;
+    const hours = minutes*60;
+    const days = hours*24
+  
+    const timeDays = Math.floor(difference/days)
+    const timeHours = Math.floor((difference%days)/hours)
+    const timeMinutes = Math.floor((difference%hours)/minutes)
+    const timeSeconds = Math.floor((difference%minutes)/seconds)
+  
+  
+    document.getElementById("days").innerHTML= timeDays;
+    document.getElementById("hours").innerHTML= timeHours ;
+    document.getElementById("minutes").innerHTML= timeMinutes;
+    document.getElementById("seconds").innerHTML= timeSeconds;
+  
+  
+  
+  
+   }
+  
+  
+  setInterval (countdown, 1000)
+  
+  
+  
+  
+  
